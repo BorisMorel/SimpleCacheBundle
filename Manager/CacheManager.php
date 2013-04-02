906fae8c477881d849a9c9d5819513c6eec1a0fd
Boris Morel <Boris.Morel@imag.fr>
2013-04-02 16:13:05 +0200

    The first logical

---
+++ Manager/CacheManager.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace IMAG\SimpleCacheBundle\Manager;
+
+class CacheManager
+{
+    
+    public function getReference($param)
+    {
+        
+    }
+}
---
+++ Util/BaseFileStorage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace IMAG\SimpleCacheBundle\Util;
+
+class BaseFileStorage implements FileStorageInterface
+{
+    
+    public function hardDriveStorage($param)
+    {
+        
+    }
+
+    public function otherStorage($param)
+}
