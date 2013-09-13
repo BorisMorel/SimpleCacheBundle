62c507259601caf067c80fe8a87227dfe141f3aa
Boris Morel <Boris.Morel@imag.fr>
2013-09-13 15:45:57 +0200

    New HddStorage

--- Storage/HddStorage.php
+++ Storage/HddStorage.php
@@ -4,19 +4,21 @@ namespace IMAG\SimpleCacheBundle\Storage;
 
 class HddStorage extends AbstractStorage
 {
-    public function getCacheFile($filename)
+    public function getCacheFile($ref)
     {
-        $cacheFile = $this->getCacheFilename($filename);
+        $cacheFile = $this->getCacheFilePath($ref);
 
-        if (!file_exists($cacheFile)) {
+        if (false === file_exists($cacheFile)) {
             return false;
         }
         
-        $stat = stat($cacheFile);
-        if ($stat['ctime'] + $this->getLifetime() < time()) {
-            unlink($cacheFile);
-            
-            return false;
+        if (0 != $this->getLifetime()) {
+            $stat = stat($cacheFile);
+            if ($stat['ctime'] + $this->getLifetime() < time()) {
+                unlink($cacheFile);
+                
+                return false;
+            }
         }
 
         $handle = fopen($cacheFile, 'r');
@@ -26,61 +28,122 @@ class HddStorage extends AbstractStorage
         return unserialize($content);
     }
 
-    public function setCacheFile($filename, $param)
+    public function setCacheFile($ref, $param)
     {
-        $cacheFile = $this->getCacheFilename($filename);
+        $path = $this->getCacheFilePath($ref);
+        
+        $pathInfo = pathinfo($path);
 
-        if (!$handle = @fopen($cacheFile, 'x')) {
+        if (false === is_dir($pathInfo['dirname'])) {
+            $this->createDirname($pathInfo['dirname']);
+        }
+        
+        if (!$handle = @fopen($path, 'x')) {
             throw new \RuntimeException(sprintf('File "%s" can\'t be open for writing', $cacheFile));
         }
-
+        
         fwrite($handle, serialize($param));
         fclose($handle);
-
+        
         return $this;
     }
 
     public function cacheClear()
     {
-        $cacheDir = $this->getCacheDir();
-        $fileExtension = $this->getFileExtension();
+        $it = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($this->getPrependPath(), \RecursiveDirectoryIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::CHILD_FIRST
+        );
 
-        $pattern = $cacheDir
+        foreach ($it as $splFileInfo) {
+            $fct = $splFileInfo->isDir() ? 'rmdir' : 'unlink';
+
+            call_user_func($fct, $splFileInfo->getRealPath());
+        }
+    }
+
+    private function getCacheFilePath($ref)
+    {
+        $hash = $this->getRefHash($ref);
+        $folders = $this->splitHash($hash);
+        
+        $path =
+            $this->getPrependPath()
             .DIRECTORY_SEPARATOR
-            .'*'
+            .implode(DIRECTORY_SEPARATOR, $folders)
+            .DIRECTORY_SEPARATOR
+            .$ref
             .'.'
-            .$fileExtension
+            .$this->getFileExtension()
             ;
+        
+        return $path;
+    }
 
-        foreach(glob($pattern) as $filename) {
-            unlink($filename);
+    private function getRefHash($ref)
+    {
+        return md5($ref);
+    }
+
+    private function splitHash($hash)
+    {
+        $maxLength = $this->getFolderLength() * $this->getDeep();
+        
+        if ($maxLength >= mb_strlen($hash)) {
+            throw new \RuntimeException("Split hash can't be performed due to a invalid folder length and deep");
+        }
+        
+        $folderPart = mb_substr($hash, 0, $maxLength);
+
+        $folders = str_split($folderPart, $this->getFolderLength());
+
+        return $folders;
+    }
+ 
+    private function getFolderLength()
+    {
+        $params = $this->getExtrasParameters();
+
+        return isset($params['folder_length']) ? (int)$params['folder_length'] : 3;
+    }
+
+    private function getDeep()
+    {
+        $params = $this->getExtrasParameters();
+
+        return isset($params['deep']) ? (int)$params['deep'] : 3;
+    }
+
+    private function getBaseDir()
+    {
+        return isset($this->configs['base_dir']) ? $this->configs['base_dir'] : '/tmp';
+    }
+
+    private function getCacheFolder()
+    {
+        return isset($this->configs['cache_folder']) ? $this->configs['cache_folder'] : 'cache_T05uXu50';
+    }
+
+    private function getFileExtension()
+    {
+        return isset($this->configs['file_extension']) ? $this->configs['file_extension'] : 'imag.cache';
+    }
+
+    private function createDirname($dir)
+    {
+        if (false === mkdir($dir, 0755, true)) {
+            throw new \RuntimeException(sprintf("Unable to create %s", $dir));
         }
 
         return true;
     }
 
-    private function getCacheFilename($filename)
+    private function getPrependPath()
     {
-        $cacheDir = $this->getCacheDir();
-        $fileExtension = $this->getFileExtension();
-
-        $file = $cacheDir
+        return 
+            $this->getBaseDir()
             .DIRECTORY_SEPARATOR
-            .$filename
-            .'.'
-            .$fileExtension
+            .$this->getCacheFolder()
             ;
-
-        return $file;
-    }
-
-    private function getCacheDir()
-    {
-        return isset($this->configs['cache_dir']) ? $this->configs['cache_dir'] : '/tmp';
-    }
-
-    private function getFileExtension()
-    {
-        return isset($this->configs['file_extension']) ? $this->configs['file_extension'] : 'bomo.cache';
     }
 }
